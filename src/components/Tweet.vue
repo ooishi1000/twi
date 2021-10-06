76fec9992687dc04fdab6768df7801a7e717ff52
ooishi1000 <karugon64@gmail.com>
2021-10-06 13:52:50 +0900

    message

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -3,6 +3,14 @@
     <div class="alert alert-info">
         <div class="alert alert-light mx-4" style="width:25rem">
             <p>{{ message }}</p>
+
+            <p><slot /></p>
+
+            <!-- <p>{{ moment }}</p> -->
+            
+            <!-- <div v-for="(rog, index) in rogs" :key="index">
+                <h5 class="card" style="text-align:left">{{rog.name}} :<p style="color:black">{{rog.talk}}</p> </h5>
+            </div> -->
         </div>
     </div>
   </div>
@@ -11,14 +19,22 @@
 <script>
 export default {
     name: 'Tweet',
-    props: ['message'],
+    props: ['message','moment'],
+    
 
     data() {
-       
+       return {
+        // rogs: [
+        // {name:'NAME', talk:'データ'},
+        // {name:'NAME', talk:'データ'},
+        // ],
+       }
     },
 
     methods: {
-
+        // plusTweet(text) {
+        //    this.$set(this.rogs,'NAME',text)
+        // }
     }
 }
 </script>
